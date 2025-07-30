09e363bc6e82dd3eb82890c8eb1aae144417dbd7
HappyPiggy <your-email@example.com>
2025-07-30 23:33:13 +0800

    fix: 本地化密码验证器，避免uni-id-pages模块依赖问题

--- pages/register/validator.js
+++ pages/register/validator.js
@@ -1,4 +1,65 @@
-import passwordMod from '@/uni_modules/uni-id-pages/common/password.js'
+// 密码验证规则（本地化版本，避免模块导入问题）
+const passwordRules = {
+	weak: /^(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z~!@#$%^&*_\-+=`|\\(){}[\]:;"'<>,.?/]{6,16}$/
+}
+
+const ERROR = {
+	normal: {
+		noPwd: '请输入密码',
+		noRePwd: '再次输入密码',
+		rePwdErr: '两次输入密码不一致'
+	},
+	passwordStrengthError: {
+		weak: '密码必须包含字母，密码长度必须在6-16位之间'
+	}
+}
+
+function validPwd(password) {
+	if (!new RegExp(passwordRules.weak).test(password)) {
+		return ERROR.passwordStrengthError.weak
+	}
+	return true
+}
+
+function getPwdRules(pwdName = 'password', rePwdName = 'password2') {
+	const rules = {}
+	rules[pwdName] = {
+		rules: [{
+				required: true,
+				errorMessage: ERROR.normal.noPwd,
+			},
+			{
+				validateFunction: function(rule, value, data, callback) {
+					const checkRes = validPwd(value)
+					if (checkRes !== true) {
+						callback(checkRes)
+					}
+					return true
+				}
+			}
+		]
+	}
+
+	if (rePwdName) {
+		rules[rePwdName] = {
+			rules: [{
+					required: true,
+					errorMessage: ERROR.normal.noRePwd,
+				},
+				{
+					validateFunction: function(rule, value, data, callback) {
+						if (value != data[pwdName]) {
+							callback(ERROR.normal.rePwdErr)
+						}
+						return true
+					}
+				}
+			]
+		}
+	}
+	return rules
+}
+
 export default {
 	"username": {
 		"rules": [{
@@ -52,5 +113,5 @@ export default {
 		],
 		"label": "昵称"
 	},
-	...passwordMod.getPwdRules()
+	...getPwdRules()
 }
